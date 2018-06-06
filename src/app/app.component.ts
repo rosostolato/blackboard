804abc3867a78a60ddf2848f54207c43c76d5166
Rosostolato <eduardo.rosostolato@fugro-br.com>
2018-06-06 17:37:27 -0300

    loading template ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public onUpload(fileList: FileList) {
+    const file = fileList[0];
+    const reader = new FileReader();
+
+    reader.onloadend = this.onLoad.bind(this);
+    reader.readAsText(file, 'ISO-8859-1');
+  }
+
+  private onLoad(e: FileReaderProgressEvent) {
+    const result: string = e.target.result;
+
+    // parse csv
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {NgxFilequakeModule} from '@ebcoder/ngx-filequake';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxFilequakeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
