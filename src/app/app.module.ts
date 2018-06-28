81d045fefa69103bd7dd06aef107ba8b9bc1f613
rosostolato <eduardo.rosostolato@gmail.com>
2018-06-27 23:18:14 -0300

    bug na produção

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,26 +11,24 @@ import { PapaParseModule } from 'ngx-papaparse';
 import { NgxFilequakeModule } from '@ebcoder/ngx-filequake';
 import { DragAndDropModule } from 'angular-draggable-droppable';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
-// import ngMediaModule from './ngMedia';
 
 // Components
 import { AppComponent } from './app.component';
 import { PeriodTableComponent } from './period-table/period-table.component';
 import { MateriasTableComponent } from './materias-table/materias-table.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { ClickEffectDirective } from './materias-table/clickEffect/click-effect.directive';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PeriodTableComponent,
     MateriasTableComponent,
-    SidebarComponent,
-    ClickEffectDirective
+    ClickEffectDirective,
+    SidebarComponent
   ],
   imports: [
     MatListModule,
-    // ngMediaModule,
     BrowserModule,
     PapaParseModule,
     DragAndDropModule,
